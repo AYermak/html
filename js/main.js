21d5bf91a10cb16d9aa956c6f4ff891db43c1493
artem <artem.ermakov.com@gmail.com>
2021-07-07 01:16:25 +0500

    Master

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ let nav = document.querySelector(".nav");
 let nav__item = document.getElementsByClassName("nav__item");
 let form__select = document.querySelector(".form__select");
 let form__svg_arrow = document.querySelector(".form__svg_arrow");
-let form__select_span = document.querySelector(".form__select_span");
+let form__select_span_1 = document.querySelector(".form__select_span_1");
+let form__select_span_2 = document.querySelector(".form__select_span_2");
+let form__select_item = document.getElementsByClassName("form__select_item");
+let form__select_input_year_hidden = document.querySelector(".form__select_input_year_hidden");
+
 header__burger.addEventListener(
     "click",
     () => {
@@ -20,7 +24,22 @@ form__select.addEventListener(
     "click",
     () => {
         form__svg_arrow.classList.toggle('form__svg_arrow_active')
+        if(form__select_span_2.innerHTML == ""){
+            form__select_span_1.classList.toggle("form__select_span_1_active");
+
+        }
     }
 )
 
-//form__select_span.innerHTML = 'lol';
+for(let i = 0; i < form__select_item.length; i++){
+    form__select_item[i].addEventListener(
+        'click',
+        () => {
+            //console.log(form__select_item[i].innerHTML);
+            form__select_span_2.style.color = "black";
+            form__select_span_2.classList.add("form__select_span_2_active");
+            form__select_span_2.innerHTML = form__select_item[i].innerHTML;
+            form__select_input_year_hidden.value = form__select_span_2.innerHTML;
+        }
+    )
+}
